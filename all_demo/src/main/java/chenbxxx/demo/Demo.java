1ff2f767b035b57525a36a342442d6c83b2d5f54
CheNbXxx <ai6547778@vip.qq.com>
2018-09-10 08:46:48 +0800

    18/9/10 modify & add
    Signed-off-by: CheNbXxx <ai6547778@vip.qq.com>

---
+++ all_demo/src/main/java/chenbxxx/demo/Demo.java
@@ -0,0 +1,12 @@
+package chenbxxx.demo;
+
+/**
+ * @author chenbxxx
+ * @email ai654778@vip.qq.com
+ * @date 2018/9/5
+ */
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld");
+    }
+}
