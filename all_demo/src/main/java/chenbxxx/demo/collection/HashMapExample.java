7983ab1e854b33e225b25ece08a6dcb04cd45e5c
CheNbXxx <ai6547778@vip.qq.com>
2018-09-30 17:32:43 +0800

    18/9/30 修改HashMapExample.javaC
    Signed-off-by: CheNbXxx <ai6547778@vip.qq.com>

--- all_demo/src/main/java/chenbxxx/demo/collection/HashMapExample.java
+++ all_demo/src/main/java/chenbxxx/demo/collection/HashMapExample.java
@@ -1,20 +1,41 @@
 package chenbxxx.demo.collection;
 
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.util.HashMap;
+
 /**
  * @author chenbxxx
  * @email ai654778@vip.qq.com
  * @date 2018/9/27
  */
 public class HashMapExample {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ClassNotFoundException {
 //        System.out.println(HashMapExample.tableSizeFor(4));
-        System.out.println(3 ^ 5);
+//        System.out.println(3 ^ 5);
+//
+//        System.out.println(Integer.toBinaryString(-8));
+//        System.out.println(Integer.toBinaryString(-8 >>> 1));
+//
+//        System.out.println(Integer.toBinaryString(8));
+//
+//        System.out.println(Integer.toBinaryString(8 >> 1));
 
-        System.out.println(Integer.toBinaryString(-8));
-        System.out.println(Integer.toBinaryString(-8 >>> 1));
+//        Class clazz = HashMap.class;
+//        Type[] genericInterfaces = clazz.getGenericInterfaces();
+//
+//        System.out.println("/******************  genericInterfaces  ******************/");
+//        for (Type type : genericInterfaces){
+//            System.out.print(type.getTypeName());
+//            if(type instanceof ParameterizedType){
+//                System.out.print(" instanceof ParameterizedType");
+//            }
+//            System.out.println("");
+//        }
 
-        System.out.println(Integer.toBinaryString(8));
-        System.out.println(Integer.toBinaryString(8 >> 1));
+        Class<TestClass> testClassClass = TestClass.class;
+        Class.forName("chenbxxx.demo.collection.TestClass");
+
     }
 
     static final int tableSizeFor(int cap) {
@@ -33,5 +54,15 @@ public class HashMapExample {
     }
     public void showName(){
         System.out.println(this.getClass().getSimpleName());
+    }
+}
+
+class TestClass{
+    static {
+        System.out.println("TestClass static code");
+}
+
+    public TestClass(){
+        System.out.println("Test Constructor code");
     }
 }
