fb099f5820863a4f9efc5fca9b492d91d72ab99a
CheNbXxx <ai6547778@vip.qq.com>
2018-11-01 18:24:32 +0800

    - -。 RabbitMQ的最基础单独应用

---
+++ all_demo/src/main/java/chenbxxx/rabbitmq/Receiving.java
@@ -0,0 +1,42 @@
+package chenbxxx.rabbitmq;
+
+import com.rabbitmq.client.*;
+import org.apache.http.pool.ConnFactory;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.concurrent.TimeoutException;
+
+/**
+ * function:
+ *
+ * @author CheNbXxx
+ * @email chenbxxx@gmail.con
+ * @date 2018/11/1 12:42
+ */
+public class Receiving {
+    private final static String QUEUE_NAME ="hello";
+
+    public static void main(String[] args) throws IOException, TimeoutException {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("118.24.134.237");
+        factory.setUsername("chen");
+        factory.setPassword("19951217");
+
+        try (Connection connection = factory.newConnection();
+             Channel channel = connection.createChannel();
+        ){
+            channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+            System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+            Consumer consumer = new DefaultConsumer(channel) {
+                @Override
+                public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body)
+                        throws IOException {
+                    String message = new String(body, StandardCharsets.UTF_8);
+                    System.out.println(" [x] Received '" + message + "'");
+                }
+            };
+            channel.basicConsume(QUEUE_NAME, true, consumer);
+        }
+    }
+}
---
+++ all_demo/src/main/java/chenbxxx/rabbitmq/Send.java
@@ -0,0 +1,34 @@
+package chenbxxx.rabbitmq;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+/**
+ * function:
+ *
+ * @author CheNbXxx
+ * @email chenbxxx@gmail.con
+ * @date 2018/11/1 12:33
+ */
+public class Send {
+    private final static String QUEUE_NAME = "hello";
+
+    public static void main(String[] args) throws IOException, TimeoutException {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("118.24.134.237");
+        factory.setUsername("chen");
+        factory.setPassword("19951217");
+
+        try(Connection connection = factory.newConnection();
+            Channel channel = connection.createChannel();) {
+            channel.queueDeclare(QUEUE_NAME,false,false,false,null);
+            String message = "HelloWorld";
+            channel.basicPublish("",QUEUE_NAME,null,message.getBytes());
+            System.out.println(" [x] Sent '" + message + "'");
+        }
+    }
+}
